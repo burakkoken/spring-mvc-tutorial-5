e8e25b87231e6b381de0f4488a1f3952eef4b1ad
Burak Köken <burakkokenn@gmail.com>
2017-01-19 20:16:07 +0200

    @RequestParam annotation'i kullanılarak get request örneği gerçekleştirildi

--- src/Controller/ContactController.java
+++ src/Controller/ContactController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import com.sun.istack.internal.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +22,19 @@ public class ContactController {
     public ModelAndView submitContactForm(@RequestParam String personName,
                                           @RequestParam String personEmail,
                                           @RequestParam String personMessage){
+        ModelAndView modelAndView = new ModelAndView("Contact/Success");
+        modelAndView.addObject("name", personName);
+        modelAndView.addObject("email", personEmail);
+        modelAndView.addObject("message", personMessage);
+
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/person", method = RequestMethod.GET)
+    public ModelAndView sendGetRequest(@RequestParam(required = false, defaultValue = "No name") String personName,
+                                       @RequestParam(required = false, defaultValue = "No Email") String personEmail,
+                                       @RequestParam(required = false, defaultValue = "No Text") String personMessage
+                                       ){
         ModelAndView modelAndView = new ModelAndView("Contact/Success");
         modelAndView.addObject("name", personName);
         modelAndView.addObject("email", personEmail);
